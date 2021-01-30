f95c84f726985f35ace4cd4dd5b243dedd7f5bd2
asukmawan <asukmawan85@gmail.com>
2021-01-30 17:25:45 -0500

    Finished mapping single points and updated notes

--- Simple_Map/static/js/logic.js
+++ Simple_Map/static/js/logic.js
@@ -12,6 +12,16 @@ let map = L.map("mapid", {
     zoom: 4
   });
 
+
+
+// // We create the tile layer that will be the background of our map.
+// let streets = L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+// attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+//     maxZoom: 18,
+//     id: 'mapbox.streets',
+//     accessToken: API_KEY
+// });
+  
 // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
